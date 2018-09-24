434261c4de5bbf353cfa4b98916144971f75f2cb
Leandro Pacheco <contact@leandropacheco.com>
2018-09-24 16:49:23 +0200

    closed loop client

--- src/client_bench/client.go
+++ src/client_bench/client.go
@@ -11,7 +11,6 @@ import (
 	"os"
 	"os/signal"
 	"state"
-	"sync"
 	"time"
 )
 
@@ -20,7 +19,7 @@ var serverPort *int = flag.Int("sport", 7070, "Port of the server to connect to"
 
 var keys *int = flag.Int("keys", 1000000, "Total number of keys")
 var nConns *int = flag.Int("c", 1, "Number of client connections to create")
-var opsSec *int = flag.Int("o", 1, "Target operations per second per client connection")
+var outstanding *int = flag.Int("o", 1, "Number of outstanding connections")
 
 var N int
 
@@ -29,7 +28,7 @@ var rsp []bool
 func main() {
 	flag.Parse()
 
-	// create nConns client connections. Each will try to reach its opsSec target.
+	// create nConns client connections. Each will have a number of outstanding operations
 	for i := 0; i < *nConns; i++ {
 		startClientConnection(fmt.Sprintf("%s:%d", *serverAddr, *serverPort))
 	}
@@ -49,52 +48,39 @@ func startClientConnection(rAddr string) {
 	writer := bufio.NewWriter(server)
 
 	pending := make(map[int32]time.Time)
-	pending_lock := new(sync.Mutex)
 
-	go clientWriter(writer, pending, pending_lock)
-	go clientReader(reader, pending, pending_lock)
-}
-
-func clientWriter(w *bufio.Writer, pending map[int32]time.Time, m *sync.Mutex) {
-	count := int64(0)
-	start := time.Now()
-
-	for {
-		time.Sleep(100 * time.Millisecond)
-		elapsed_ms := time.Since(start) / time.Millisecond
-		target := (elapsed_ms * (time.Duration(*opsSec))) / 1000
-		for ; count < int64(target); count++ {
-			now := time.Now()
-			id := rand.Int31()
-			key := rand.Int63n(int64(*keys))
-			val := 0
-			args := genericsmrproto.Propose{id, state.Command{state.PUT, state.Key(key), state.Value(val)}, 0}
-			m.Lock()
-			pending[id] = now
-			m.Unlock()
-			w.WriteByte(genericsmrproto.PROPOSE)
-			args.Marshal(w)
-			w.Flush()
-		}
+	// send outstanding msgs
+	for count := 0; count < *outstanding; count++ {
+		propose(pending, writer)
 	}
-}
 
-func clientReader(r *bufio.Reader, pending map[int32]time.Time, m *sync.Mutex) {
+	// wait for replies and propose again
 	for {
 		reply := new(genericsmrproto.ProposeReplyTS)
-		if err := reply.Unmarshal(r); err != nil {
+		if err := reply.Unmarshal(reader); err != nil {
 			log.Println("Error reading: ", err)
 			continue
 		}
 		if reply.OK != 0 {
-			m.Lock()
 			start := pending[reply.CommandId]
 			delete(pending, reply.CommandId)
 			dur_us := time.Since(start) / time.Microsecond
 			fmt.Printf("%d %d l 0\n", start.UnixNano()/1000, dur_us)
-			m.Unlock()
 		} else {
 			log.Println("Reply error: ", reply.OK)
 		}
+		// send another msg
+		propose(pending, writer)
 	}
+}
+
+func propose(pending map[int32]time.Time, w *bufio.Writer) {
+	id := rand.Int31()
+	key := rand.Int63n(int64(*keys))
+	val := 0
+	args := genericsmrproto.Propose{id, state.Command{state.PUT, state.Key(key), state.Value(val)}, 0}
+	pending[id] = time.Now()
+	w.WriteByte(genericsmrproto.PROPOSE)
+	args.Marshal(w)
+	w.Flush()
 }
